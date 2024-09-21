3ad2599c8fc619a709fceaa66bd47c0634c9d841
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-09-21 19:34:55 +0900

    文字から音声を生成して、音声を再生することができました

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import Image from "next/image";
 import styles from "./page.module.css";
 import axios from "axios"
+import _ from 'lodash';
 
 export default function Home() {
   const voiceVoxRootUrl = "http://localhost:50021"
@@ -11,12 +11,29 @@ export default function Home() {
     const speackersResponse = await axios.get(`${voiceVoxRootUrl}/speakers`)
     console.log(speackersResponse)
     const targetSpeacker = speackersResponse.data.find((speacker) => speacker.name === targetSpeackerName)
+    const targetSpeackerStyle = _.sample(targetSpeacker.styles);
+    const speackerId = targetSpeackerStyle.id;
     console.log(targetSpeacker)
     const responseAudio = await axios.post(`${voiceVoxRootUrl}/audio_query`, null, {params: {
-      text: "こんにちは",
-      speaker: 3,
+      text: "パンツ見せてくれませんか?",
+      speaker: speackerId,
     }})
-    console.log(responseAudio)
+    console.log(responseAudio.data)
+    const responseSynthesis = await axios.post(
+      `${voiceVoxRootUrl}/synthesis`,
+      responseAudio.data,
+      {
+        responseType: 'blob',
+        params: {
+          speaker: speackerId,
+        }
+      },
+    )
+    const audioBlob = responseSynthesis.data
+    const audioUrl = URL.createObjectURL(audioBlob)
+    const audio = new Audio(audioUrl)
+    audio.volume = 1
+    audio.play()
   }
 
   return (
