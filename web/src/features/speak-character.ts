61ca634bc49403909c487982a57bb6b43e33b178
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-09-23 01:25:01 +0900

    speakメソッドの整理としっかりawaitする

--- web/src/features/speak-character.ts
+++ web/src/features/speak-character.ts
@@ -1,28 +1,26 @@
 import axios from 'axios';
+import { EmotionType } from './vrmViewer/model';
 import { Viewer } from './vrmViewer/viewer';
 
-export async function speakCharacter(speakText: string, viewer: Viewer) {
+export async function speakCharacter(speakText: string, viewer: Viewer, expression: EmotionType = 'neutral'): Promise<void> {
   const voiceVoxRootUrl = process.env.NEXT_PUBLIC_VOICEVOX_API_ROOT_URL;
   const targetSpeackerName = 'ずんだもん';
   const speackersResponse = await axios.get(`${voiceVoxRootUrl}/speakers`);
-  console.log(speackersResponse);
   const targetSpeacker = speackersResponse.data.find((speacker: any) => speacker.name === targetSpeackerName);
   // 'ノーマル', 'あまあま', 'ツンツン', 'セクシー', 'ささやき', 'ヒソヒソ' がある
   const targetSpeackerStyle = targetSpeacker.styles.find((style: any) => style.name === 'あまあま');
   const speackerId = targetSpeackerStyle.id;
-  console.log(targetSpeacker);
   const responseAudio = await axios.post(`${voiceVoxRootUrl}/audio_query`, null, {
     params: {
       text: speakText,
       speaker: speackerId,
     },
   });
-  console.log(responseAudio.data);
   const responseSynthesis = await axios.post(`${voiceVoxRootUrl}/synthesis`, responseAudio.data, {
     responseType: 'arraybuffer',
     params: {
       speaker: speackerId,
     },
   });
-  viewer.model?.speak(responseSynthesis.data, 'neutral');
+  return viewer.model?.speak(responseSynthesis.data, expression);
 }
